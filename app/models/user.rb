3742f8546b85da4b7bbd19fcd08375831470c266
cjmanetta & Daniel Shafer <pair+cjmanetta.dannyshafer@devbootcamp.com>
2015-07-16 10:46:21 -0700

    do work

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ActiveRecord::Base
+  include BCrypt
+
+   def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(username, password)
+    user = User.find_by(username: username)
+    if user && user.password == password
+      user
+    end
+  end
+end
