16dd7a7677fd3e21763b08766070b079d626703a
cjmanetta & Daniel Shafer <pair+cjmanetta.dannyshafer@devbootcamp.com>
2015-07-16 21:41:21 -0700

    starting ajax and more formatting

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,16 +1,23 @@
+$(document).ready(function() {
+    bindEvents();
+});
+
+
 $("#login_button").click(function(){
     $("#modal").show();
 });
 
-$("#signup").on("click", function(e) {
-    event.preventDefault();
-    var url = '/users'
-    var superRequest = $.ajax({
-        type: "post",
-        url: url,
-        data: formData
-    })
-    superRequest.done({
+function bindEvents() {
+    $("#signup").on("submit", function(e) {
+        event.preventDefault();
+        var url = '/users'
+        var superRequest = $.ajax({
+            type: "post",
+            url: url,
+            data: $(this).serialize(),
+        })
+        superRequest.done({
 
+        })
     })
-})
+};
