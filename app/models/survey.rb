b64dc05247728f99ff9f874b2690f83b04663884
cjmanetta & Daniel Shafer <pair+cjmanetta.dannyshafer@devbootcamp.com>
2015-07-17 12:50:04 -0700

    add get route for take survey

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,6 +3,7 @@ class Survey < ActiveRecord::Base
   validates :name, uniqueness: true, presence: true
   belongs_to :author, class_name: "User"
   has_many :user_surveys
+  has_many :questions
 
   has_many :takers, through: :user_surveys
   # has_many :users, through: :user_surveys
