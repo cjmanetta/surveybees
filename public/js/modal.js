4ba0956ef6e14e7ade3ba9bfaeaa393dfee37998
cjmanetta & Daniel Shafer <pair+cjmanetta.dannyshafer@devbootcamp.com>
2015-07-17 10:26:27 -0700

    fix small things on layout

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,23 +1,4 @@
-$(document).ready(function() {
-    bindEvents();
-});
-
-
 $("#login_button").click(function(){
     $("#modal").show();
 });
 
-function bindEvents() {
-    $("#signup").on("submit", function(e) {
-        event.preventDefault();
-        var url = '/users'
-        var superRequest = $.ajax({
-            type: "post",
-            url: url,
-            data: $(this).serialize(),
-        })
-        superRequest.done({
-
-        })
-    })
-};
